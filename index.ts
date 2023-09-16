1be678a3ee925c27ececdf99ee576fc18f3f1ce5
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-09-16 12:27:49 -0400

    server works

--- index.ts
+++ index.ts
@@ -1,17 +1,35 @@
 import cohere from "cohere-ai";
-
-const direction = "at the floor with awkwardness."
-
 cohere.init(process.env.COHERE_API_KEY as string);
-const response = await cohere.generate({
-  model: "command",
-  prompt: `I am building a hackathon project that berates user if they don't look into someones eye while talking to them. You are to generate the berating text. Right now the user is looking ${direction}`,
-  max_tokens: 50,
-  temperature: 1
-})
 
-if (response.statusCode != 200) {
-  console.log("\n\n-----\nGeneration failed.\n\n-----\n");
+async function generateBerateText(direction: string): Promise<Error | string> {
+  const response = await cohere.generate({
+    model: "command",
+    prompt: `I am building a hackathon project that berates user in a funny way if they don't look into someones eye while talking to them. You are to generate the funny berating text. Right now the user is looking ${direction}`,
+    max_tokens: 50,
+    temperature: 1
+  })
+
+  if (response.statusCode != 200) {
+    console.log("\n\n-----\nGeneration failed.\n\n-----\n");
+    return new Error("Generation failed.");
+  }
+
+  return response.body.generations[0].text;
 }
 
-console.log(response.body.generations[0].text);
+Bun.serve({
+  port: 3000,
+  async fetch(req) {
+    const url = new URL(req.url);
+    const direction = url.searchParams.get("q") || "down at the floor";
+    console.log("Search params: ", direction);
+
+    const text = await generateBerateText(direction);
+    if (text instanceof Error) {
+      return new Response("error");
+    }
+
+    console.log(text);
+    return new Response(text);
+  },
+});
